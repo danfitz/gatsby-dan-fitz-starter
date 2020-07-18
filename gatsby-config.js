26a81501b6813069622be3ac66d40c6d0032da8c
Dan Fitz <dan@danfitz.com>
2020-07-18 14:34:52 -0400

    Initialize repo

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-styled-components',
+      options: {
+        // Add any options here
+        // See https://styled-components.com/docs/tooling#babel-plugin for list of options
+      },
+    },
+  ],
+};
