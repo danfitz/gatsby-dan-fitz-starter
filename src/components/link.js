73ed90a6c7ffe5958d1aa3e90e6d78b29816a2f6
Dan Fitz <dan@danfitz.com>
2020-08-08 12:27:21 -0400

    Changed logic check for link

--- src/components/link.js
+++ src/components/link.js
@@ -6,17 +6,19 @@ import { Link as GatsbyLink } from 'gatsby';
 const Link = ({ to, children, ...props }) => {
   // Gatsby's Link component only supports relative paths, so we need to perform a logic check first
   // https://www.gatsbyjs.org/docs/gatsby-link/#reminder-use-link-only-for-internal-links
-  const isExternal = typeof to === 'string' && to.startsWith('http');
+  // This test assumes all internal links are either (1) an object or (2) start with exactly one slash
+  const isInternal =
+    typeof to === 'object' || (typeof to === 'string' && /^\/(?!\/)/.test(to));
 
-  if (isExternal) {
+  if (isInternal) {
     return (
-      <RebassLink href={to} {...props}>
+      <RebassLink as={GatsbyLink} to={to} {...props}>
         {children}
       </RebassLink>
     );
   } else {
     return (
-      <RebassLink as={GatsbyLink} to={to} {...props}>
+      <RebassLink href={to} {...props}>
         {children}
       </RebassLink>
     );
