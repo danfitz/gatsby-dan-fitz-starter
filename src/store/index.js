3147dc3e3a8008e38a1d9c1076740b75da43edea
Dan Fitz <dan@danfitz.com>
2020-09-20 13:27:10 -0400

    Added redux devtools connection

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { createStore as reduxCreateStore } from 'redux';
 import rootReducer from './reducers';
 
-const createStore = () => reduxCreateStore(rootReducer);
+const createStore = () =>
+  reduxCreateStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 export default createStore;
